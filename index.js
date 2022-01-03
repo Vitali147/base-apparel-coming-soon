d8db2e5c320171ae5b901632e078b92f15541d3d
Vitali147 <vitali.rjabkov@gmail.com>
2022-01-03 11:55:21 +0000

    complete project

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-function validation(){
-    let form = document.getElementById("form");
-    let email = document.getElementById("email").value;
-    let text = document.getElementById("text");
+const email = document.getElementById("email");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+const exclamation = document.getElementById("excl-mark");
 
-    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    if (email.match(pattern)){
-        form.classList.add("valid");
-        form.classList.remove("invalid");
-        ext.innerHTML = "";
-
-    }else{
-        form.classList.remove("valid");
-        form.classList.add("invalid");
-        text.innerHTML = "Please provide a valid email";
+email.onkeydown = function(){
+    const regex = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,8}$/;
+    const regexo = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,3}\.[a-zA-Z]{1,3}$/;
+    
+    if(regex.test(email.value) || regexo.test(email.value)){
+        errorElement.innerText = "";
+        exclamation.style.display = "none";
+    }
+    else{
+        errorElement.innerText = "Please provide a valid email";
+        errorElement.style.color = "hsl(0, 93%, 68%)";
+        exclamation.style.display = "block";
     }
 }
